0f154d13829cba22bbbc4f6170df426ee918b30b
Ágata Cecília Silva Cesquim <129690783+AgataCeci@users.noreply.github.com>
2024-05-28 22:28:46 -0300

    Create main.c

---
+++ B2-6/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int seqNum[5] = {10,25,40,80};
+  int valor = 30;
+  int numElementos = 4;
+
+  int meio = numElementos/2;
+
+  for(int i=numElementos; i > meio; i--){
+    seqNum[i] = seqNum[i-1];
+  }
+
+  seqNum[meio] = valor;
+
+  for (int i=0; i<5; i++){
+    printf("%d\n", seqNum[i]);
+  }
+
+ return 0;
+  
+}
