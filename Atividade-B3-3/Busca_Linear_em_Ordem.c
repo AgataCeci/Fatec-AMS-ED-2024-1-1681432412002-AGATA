7acf0602083ad8acc0650b1091adb69b7fdfb2b1
Ágata Cecília Silva Cesquim <129690783+AgataCeci@users.noreply.github.com>
2024-10-01 15:16:56 -0300

    Create Busca_Linear_em_Ordem.c

---
+++ Atividade-B3-3/Busca_Linear_em_Ordem.c
@@ -0,0 +1,33 @@
+/*------------------------------------------------------------------------*/
+/*   FATEC-São Caetano do Sul                 Estrutura de Dados          */
+/*                                                                        */
+/*                         Ágata Cesquim                                  */
+/*     Objetivo: Analisar eficiência de algorítimos e passar para         */
+/*               notação Big O                                            */
+/*                                                                        */
+/*                                                                        */
+/*                                                        Data:01/10/2024 */
+/*------------------------------------------------------------------------*/ 
+
+#include <stdio.h>
+
+int linearSearchOrdered(int arr[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x)
+            return i; // Element found
+        else if (arr[i] > x)
+            break; // Element is not in the array due to sorted nature
+    }
+    return -1; // Element not found
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 10, 40};
+    int x = 5;
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int result = linearSearchOrdered(arr, n, x);
+    (result == -1) ? printf("Element not present\n")
+                   : printf("Element found at index %d\n", result);
+    return 0;
+}
